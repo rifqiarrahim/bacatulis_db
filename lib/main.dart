7dea897bca88c4fc67c7be5bee036aee07e78e98
rifqiarrahim <mrifqian@gmail.com>
2021-08-22 01:34:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bacatulisfile/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Flutter Demo',
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.systemBlue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
